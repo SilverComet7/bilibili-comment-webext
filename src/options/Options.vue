1cc687a231453ea8bac0a3304c959e5e2b64a8d6
SilverComet7 <841531691@qq.com>
2022-10-31 15:56:29 +0800

    todo:核心复制粘贴能力，chrome storage 和localStorage区分

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
+// import { storageDemo } from '~/logic/storage'
+const list = reactive({ t1: 'xx', t2: '123' })
 </script>
 
 <template>
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <img src="/assets/icon.svg" class="icon-btn mx-2 text-2xl" alt="extension icon">
-    <div>Options</div>
-    <p class="mt-2 opacity-50">
-      This is the options page
-    </p>
-
-    <input v-model="storageDemo" class="border border-gray-400 rounded px-2 py-1 mt-2">
-
-    <div class="mt-4">
-      Powered by Vite <pixelarticons-zap class="align-middle" />
+    <div v-for="(value, key) in list" :key="value" class="my-2">
+      {{ value }}---------{{ key }}
     </div>
+    <!-- <div class="opacity-0">
+      Storage:{{ storageDemo[0].t1 }}
+    </div>
+    <div v-for="(item) in storageDemo" :key="item.t1" class="my-2">
+      {{ item.t1 }}
+    </div> -->
   </main>
 </template>
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,23 +1,39 @@
 <script setup lang="ts">
+// import { useClipboard } from '@vueuse/core'
 import { storageDemo } from '~/logic/storage'
+const showCopyWord = ref('123')
 
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
+}
+
+function copyComment(comment: string) {
+  const replacedComment = comment.replace('XX', '九川')
+  showCopyWord.value = replacedComment
+  // const { text, copy, copied, isSupported } = useClipboard({ source: replacedComment })
+  // console.log('🚀 ~ file: Popup.vue ~ line 15 ~ copy ~ text, copy, copied, isSupported ', text, copied, isSupported)
+  // copy()
 }
 </script>
 
 <template>
   <main class="w-[300px] px-4 py-5 text-center text-gray-700">
-    <Logo />
-    <div>Popup</div>
-    <p class="mt-2 opacity-50">
-      This is the popup page
-    </p>
     <button class="btn mt-2" @click="openOptionsPage">
       Open Options
     </button>
     <div class="mt-2">
-      <span class="opacity-50">Storage:</span> {{ storageDemo }}
+      {{ showCopyWord }}
+      <div class="opacity-0">
+        Storage:{{ storageDemo[0].tabName }}
+      </div>
+      <div v-for="(item) in storageDemo" :key="item.tabName" class="my-2">
+        <div v-for="(subItem) in item.children" :key="subItem.comment" class="my-2">
+          {{ subItem.comment }}:
+          <button class="btn mt-2" @click="copyComment(subItem.comment)">
+            复制
+          </button>
+        </div>
+      </div>
     </div>
   </main>
 </template>
